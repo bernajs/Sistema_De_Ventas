13ad22fc8532d84b79e82f080fd04bababd7d1dd
Lalodc <laloodc@hotmail.com>
2015-11-13 09:05:11 -0600

    Modificación de la vista modificar_cliente
    Se quitaron algunos elementos de la vista, para mostrar en el modal

--- views/Cliente/modificar_cliente.php
+++ views/Cliente/modificar_cliente.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml"><head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrar Cliente</title>
-	<!-- BOOTSTRAP STYLES-->
-    <link href="<?php echo URL_BASE."/public/assets/css/bootstrap.css";?>" rel="stylesheet">
-     <!-- FONTAWESOME STYLES-->
-    <link href="<?php echo URL_BASE."/public/assets/css/font-awesome.css";?>" rel="stylesheet">
-        <!-- CUSTOM STYLES-->
-    <link href="<?php echo URL_BASE."/public/assets/css/custom.css";?>" rel="stylesheet">
-     <!-- GOOGLE FONTS-->
-   <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
-
-</head>
-<body>
-    <div class="container">
+    
         <div class="row text-center  ">
             <div class="col-md-12">
                 <h2>Modificar cliente</h2>
@@ -28,7 +10,7 @@
          
          <div class="row">
                
-                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                <div class="col-md-12 col-sm-12 ">
                         <div class="panel panel-default">
                             <div class="panel-heading">
                         <strong>Cliente</strong>  
@@ -38,10 +20,7 @@
                                 <form role="form" class="form" id="form1" method="POST" action="">
                                         <div class="form-group input-group <?php if (isset($this->errores['nombre'])) echo 'has-error' ; ?>">
                                             <span class="input-group-addon"><i class="fa fa-genderless"></i></span>
-                                            <input class="form-control" name="nombre" id="nombre" placeholder="Nombre"             <?php
-                 $inputu=$_POST['user'];
-                 print $inputu;
-                 ?>" type="text">
+                                            <input class="form-control" name="nombre" id="nombre" placeholder="Nombre"  type="text">
                                             <?php if(isset($this->errores['nombre'])) :?> <span id="helpBlock" class="help-block"><?php echo $this->errores['nombre'];?></span><?php endif;?>
                                         </div>
                                         <div class="form-group input-group <?php if (isset($this->errores['aPaterno'])) echo 'has-error' ; ?>">
@@ -126,29 +105,3 @@
                 
                 
         </div>
-    </div>
-
-
-     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
-    <!-- JQUERY SCRIPTS -->
-    <script src="//www.google-analytics.com/analytics.js" async=""></script><script src="assets/js/jquery-1.10.2.js"></script>
-      <!-- BOOTSTRAP SCRIPTS -->
-    <script src="<?php echo URL_BASE."/public/assets/js/bootstrap.min.js";?>"></script>
-    <!-- METISMENU SCRIPTS -->
-    <script src="<?php echo URL_BASE."/public/assets/js/jquery.metisMenu.js";?>"></script>
-      <!-- CUSTOM SCRIPTS -->
-    <script src="<?php echo URL_BASE."/public/assets/js/custom.js";?>"></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-38955291-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
-
-</body>
-</html>
