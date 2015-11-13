4184adb2ea9f9961d2e9e5722c7d8a1f14123e3f
Lalodc <laloodc@hotmail.com>
2015-11-13 13:41:23 -0600

    Modificación en la vista errorPage
    Se le quitó el div class "container"

--- views/errorPage.php
+++ views/errorPage.php
@@ -18,7 +18,7 @@
         </head>
     <body>
 
-<div class="container">
+
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="box">
@@ -39,7 +39,7 @@
         
         
 	</div>
-</div>
+
 
 
     </body>
