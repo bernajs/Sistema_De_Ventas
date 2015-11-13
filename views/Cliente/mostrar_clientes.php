ac8950fb59b393c0efcbea6ca13ce3aecfe88931
Lalodc <laloodc@hotmail.com>
2015-11-13 10:11:12 -0600

    Cambios en la vista mostrar_cliente
    Se quitó el texto "Edit your email" y se quito el botón de Update de
    esta vista

--- views/Cliente/mostrar_clientes.php
+++ views/Cliente/mostrar_clientes.php
@@ -305,7 +305,7 @@ font-size: 16px"> Último acceso : 30 May 2014 &nbsp; <a style="border-radius: 5
     <div class="modal-content">
           <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-        <h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
+        <!--<h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>-->
       </div>
           <div class="modal-body" id="modal-edit">
           <div class="form-group">            
@@ -321,9 +321,9 @@ font-size: 16px"> Último acceso : 30 May 2014 &nbsp; <a style="border-radius: 5
         
         </div>
       </div>
-          <div class="modal-footer ">
-        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
-      </div>
+          <!--<div class="modal-footer ">-->
+        <!--<button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>-->
+      <!--</div>-->
         </div>
     <!-- /.modal-content --> 
   </div>
