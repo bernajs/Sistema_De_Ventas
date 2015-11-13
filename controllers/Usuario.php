e0b37b8a735c37de2287f23bec4a0b34bf59d5f1
Lalodc <laloodc@hotmail.com>
2015-11-13 14:00:23 -0600

    Agregó la lib View y se llamó al método loadModel

--- controllers/Usuario.php
+++ controllers/Usuario.php
@@ -1,12 +1,13 @@
 <?php
 	namespace controllers;	
 	use libs\Controller;
+	use libs\View;
 
 	class Usuario extends Controller{
 
 		public function __construct(){
 			parent::__construct();
-			
+			$this->loadModel();			
 		}
 
 		public function usuario_inicio(){
