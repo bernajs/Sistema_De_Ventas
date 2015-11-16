40482e51257ca402246adb7e2c64aa83c0168202
Lalodc <laloodc@hotmail.com>
2015-11-15 22:02:23 -0600

    Se modificó el modelo Producto
    Se agregó el método listar proveedores, para mostrar los proveedores que
    abastecen algún producto

--- models/ProductoModel.php
+++ models/ProductoModel.php
@@ -4,5 +4,15 @@
 	use libs\DBConexion;
 
 	class ProductoModel{
-		
+
+		public function listarInventarios(){
+			$con = DBConexion::getInstance();
+			if (is_null($con)) {
+				throw new Exception("Error en la conexion a la base de datos, verifique",1);
+			}
+
+			$proveedores = $con->executeQuery('SELECT * FROM proveedor;',null, __NAMESPACE__.'\ProductoModel');
+
+			return $proveedores;
+		}
 	}
