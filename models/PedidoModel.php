753f1a657d954cbfc42294dec6750ac4ca11be86
Lalodc <laloodc@hotmail.com>
2015-11-17 14:51:18 -0600

    Se modificó el modelo Pedido
    Se agregaron los métodos correspondientes para llamar los clientes y los
    productos para llenar un pedido

--- models/PedidoModel.php
+++ models/PedidoModel.php
@@ -4,5 +4,26 @@
 	use libs\DBConexion;
 
 	class PedidoModel{
+		public function listarInventarios(){
+			$con = DBConexion::getInstance();
+			if (is_null($con)) {
+				throw new Exception("Error en la conexion a la base de datos, verifique",1);
+			}
+
+			$clientes = $con->executeQuery('SELECT * FROM cliente;',null, __NAMESPACE__.'\PedidoModel');
+
+			return $clientes;
+		}
+
+		public function listarProductos(){
+			$con = DBConexion::getInstance();
+			if (is_null($con)) {
+				throw new Exception("Error en la conexion a la base de datos, verifique",1);
+			}
+
+			$productos = $con->executeQuery('SELECT * FROM producto;',null, __NAMESPACE__.'\PedidoModel');
+
+			return $productos;
+		}
 		
 	}
