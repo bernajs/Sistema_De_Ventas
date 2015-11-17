937a71b207c38794754ac1c9d5474da6732d7bc3
Lalodc <laloodc@hotmail.com>
2015-11-16 23:37:19 -0600

    Se modificó el modelo Producto
    Se agregaron los métodos necesarios para insertar un producto

--- models/ProductoModel.php
+++ models/ProductoModel.php
@@ -5,6 +5,12 @@
 
 	class ProductoModel{
 
+		public $nombre;
+		public $precioUnitario;
+		public $descripcion;
+		public $cantidad;
+		public $PROVEEDOR_rfc;
+
 		public function listarInventarios(){
 			$con = DBConexion::getInstance();
 			if (is_null($con)) {
@@ -14,5 +20,34 @@
 			$proveedores = $con->executeQuery('SELECT * FROM proveedor;',null, __NAMESPACE__.'\ProductoModel');
 
 			return $proveedores;
+		}
+
+		public function crearInventario($nombre,$precioUnitario,$descripcion,$cantidad,$PROVEEDOR_rfc){
+			$this->nombre = $nombre;
+			$this->precioUnitario = $precioUnitario;
+			$this->descripcion = $descripcion;
+			$this->cantidad = $cantidad;
+			$this->PROVEEDOR_rfc = $PROVEEDOR_rfc;
+
+			echo "jiji";
+			$this->guardar();
+		}
+
+		public function guardar(){
+			$con = DBConexion::getInstance();
+			$params = array(
+					$this->nombre,
+					$this->precioUnitario,
+					$this->descripcion,
+					$this->cantidad,
+					$this->PROVEEDOR_rfc
+				);
+
+			$sql = vsprintf("INSERT INTO producto(nombre,precioUnitario,descripcion,cantidad,PROVEEDOR_rfc) VALUES('%s', %s, '%s',%s,%s);", $params);
+			echo $sql;
+			$con->executeUpdate(array($sql));
+
+			echo "PASOO TODOO!";
+
 		}
 	}
