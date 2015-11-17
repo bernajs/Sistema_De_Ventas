8537e920963d9bbbfd339650ed944acf1995b1f0
Lalodc <laloodc@hotmail.com>
2015-11-17 14:51:54 -0600

    Se modificó el controlador Pedido
    Se agregó y modificó el método guardar_pedido(), para poder guardar un
    pedido. Queda pendiente guardar_pedido()

--- controllers/Pedido.php
+++ controllers/Pedido.php
@@ -17,9 +17,22 @@
 
 		}
 
-		public function guardar_pedido(){			
-			$this->view->render(explode("\\",get_class($this))[1], "guardar_pedido",$this->getErrores());			
+		public function guardar_pedido(){				
+			$clientes=$this->model->listarInventarios();
+			$productos=$this->model->listarProductos();
+			echo "lol";
+			//print_r($clientes);
+			//$this->view->render(explode("\\",get_class($this))[1], "guardar_producto",$proveedores,$this->getErrores());			
+			//$this->guardar($params);
+			if(isset($params['dni']) && isset($params['formaPago']) && isset($params['fecha']) && isset($params['estado']) && isset($params['direccionEnvio']) && isset($params['PRODUCTO_codigo']) && isset($params['cantProducto']) ) {
+				//$this->guardarProducto($params);
+				echo "lol";
+				print_r($params);
+				//$this->guardarProducto($params);
+				//$proveedores=$this->model->listarInventarios();
+				//$this->crearProveedor($params);
+				
+			}
+			$this->view->render2(explode("\\",get_class($this))[1], "guardar_pedido",$clientes,$productos,$this->getErrores());			
 		}
-
-
-	}
+	}	
