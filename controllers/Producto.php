d17764e0ad948e573dd4ba025927f7a89475431c
Lalodc <laloodc@hotmail.com>
2015-11-16 23:38:17 -0600

    Se modificó el controlador Producto
    Se editaron y se agregaron los métodos necesarios para registrar un
    producto. registrar producto funcionando...

--- controllers/Producto.php
+++ controllers/Producto.php
@@ -18,22 +18,41 @@
 
 		}
 
-		public function guardar_producto(){
+		public function guardar_producto($params=array()){
 			$proveedores=$this->model->listarInventarios();
+			//$this->view->render(explode("\\",get_class($this))[1], "guardar_producto",$proveedores,$this->getErrores());			
+			//$this->guardar($params);
+			if(isset($params['nombre']) && isset($params['precioUnitario']) && isset($params['descripcion']) && isset($params['cantidad']) && isset($params['PROVEEDOR_rfc'])) {
+				//$this->guardarProducto($params);
+				echo "lol";
+				print_r($params);
+				$this->guardarProducto($params);
+				//$proveedores=$this->model->listarInventarios();
+				//$this->crearProveedor($params);
+				
+			}
 			$this->view->render(explode("\\",get_class($this))[1], "guardar_producto",$proveedores,$this->getErrores());			
-			//$this->guardar($params=array());
+
 
 		}
 
-		/*public function guardar(){
-			if(isset($params['rfc']) && isset($params['nombre']) && isset($params['telefono']) && isset($params['representanteLegal']) && isset($params['fax']) && isset($params['correoElectronico']) && isset($params['webProveedor']) && isset($params['observaciones']) && isset($params['ciudad']) && isset($params['cp']) && isset($params['colonia']) && isset($params['calle']) && isset($params['numero']) && isset($params['detalle'])){
-				$this->guardarDireccion($params1);
+		public function guardarProducto($params){
+			$nombre = $params['nombre'];
+		    $precioUnitario = $params['precioUnitario'];
+		    $descripcion = $params['descripcion'];
+		    $cantidad = $params['cantidad'];
+		    $PROVEEDOR_rfc = $params['PROVEEDOR_rfc'];
 
-				print_r($params);
-				$this->crearProveedor($params);
-				
-			}
-		}*/
+		    if(count($this->errores) ==0 ){
+		    	try{
+		        	$this->model->crearInventario($nombre,$precioUnitario,$descripcion,$cantidad,$PROVEEDOR_rfc);
+		    	}
+		    	catch(\Exception $e){
+					$this->errores['global']=$e->getMessage();
+				}
+		    }
+			
+		}
 
 		/*public function mostrar_proveedores(){
 			$proveedores=$this->model->listarInventarios();
