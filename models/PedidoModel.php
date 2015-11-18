b7611a8e4ddb4a3528af542f917d81fc39c76d41
Lalodc <laloodc@hotmail.com>
2015-11-17 22:50:08 -0600

    Se modificó el modelo Pedido
    Se agregaron los métodos necesarios para registrar un pedido

--- models/PedidoModel.php
+++ models/PedidoModel.php
@@ -4,6 +4,12 @@
 	use libs\DBConexion;
 
 	class PedidoModel{
+		public $formaPago;
+		public $fecha;
+		public $estado;
+		public $direccionEnvio;
+		public $CLIENTE_dni;
+
 		public function listarInventarios(){
 			$con = DBConexion::getInstance();
 			if (is_null($con)) {
@@ -24,6 +30,33 @@
 			$productos = $con->executeQuery('SELECT * FROM producto;',null, __NAMESPACE__.'\PedidoModel');
 
 			return $productos;
+		}
+
+		public function registrarPedido($formaPago,$fecha,$estado,$direccionEnvio,$CLIENTE_dni){
+			$this->formaPago = $formaPago;
+			$this->fecha = $fecha;
+			$this->estado = $estado;
+			$this->direccionEnvio = $direccionEnvio;
+			$this->CLIENTE_dni = $CLIENTE_dni;
+
+			$this->guardar();
+		}
+
+		public function guardar(){
+			$con = DBConexion::getInstance();
+			$params = array(
+					$this->formaPago,
+					$this->fecha,
+					$this->estado,
+					$this->direccionEnvio,
+					$this->CLIENTE_dni
+				);
+
+			$sql = vsprintf("INSERT INTO pedido(formaPago,fecha,estado,direccionEnvio,CLIENTE_dni) VALUES('%s', '%s', '%s','%s',%s);", $params);
+			echo $sql;
+			$con->executeUpdate(array($sql));
+			echo "PASOOOTOOODO!";
+
 		}
 		
 	}
