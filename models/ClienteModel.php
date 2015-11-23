ea5e5080bcb44a5cb269eb72aef6c9b0b78225b8
Lalodc <laloodc@hotmail.com>
2015-11-22 23:35:57 -0600

    Modificación al modelo Cliente
    Se comentó código innecesario

--- models/ClienteModel.php
+++ models/ClienteModel.php
@@ -49,7 +49,7 @@ class ClienteModel {
 		$this->update();
 	}
 	catch (Exception $e) {
-		throw $e;	
+			throw $e;
 	}
 	}
 	public function update(){
@@ -77,48 +77,44 @@ class ClienteModel {
 				$con->executeUpdate(array($sql2));
 			}
 			catch (Exception $e) {
-				throw $e;	
+					throw $e;
 			}
 	}
 	public function eliminarCliente($id){
 		try {
 			//$dni = $id;
-			echo "<script language='javascript'>";
-			echo "alert('$id')";
-			echo "</script>";
-			echo "<script language='javascript'>";
-			echo "alert('HOLAA')";
-			echo "</script>";
-			$con = DBConexion::getInstance();
-
-			if (is_null($con)) {
-			throw new Exception("Error en la conexion a la base de datos, verifique",1);
-			}
-
-			//$cliente = $con->executeQuery("DELETE FROM cliente WHERE dni = ?;",array($id), __NAMESPACE__.'\ClienteModel');
-			//$sql="DELETE FROM cliente where dni = $id;";
-			$sql1 = vsprintf("DELETE FROM cliente WHERE dni = %s", $id);
-			//echo $sql;
-			$con->executeUpdate(array($sql1));
-			echo "<script language='javascript'>";
-						echo "alert('$sql1')";
-			echo "</script>";
-			//$con->executeQuery($sql);
-			echo "<script language='javascript'>";
-			echo "alert('PASOO')";
-			echo "</script>";
-			
-		} catch (Exception $e) {
-			throw $e;
-		}
-		
+//echo "<script language='javascript'>";
+//echo "alert('$id')";
+//echo "</script>";
+//echo "<script language='javascript'>";
+//echo "alert('HOLAA')";
+//echo "</script>";
+$con = DBConexion::getInstance();
+if (is_null($con)) {
+throw new Exception("Error en la conexion a la base de datos, verifique",1);
+}
+//$cliente = $con->executeQuery("DELETE FROM cliente WHERE dni = ?;",array($id), __NAMESPACE__.'\ClienteModel');
+//$sql="DELETE FROM cliente where dni = $id;";
+$sql1 = vsprintf("DELETE FROM cliente WHERE dni = %s", $id);
+//echo $sql;
+$con->executeUpdate(array($sql1));
+//echo "<script language='javascript'>";
+						//			echo "alert('$sql1')";
+//echo "</script>";
+//$con->executeQuery($sql);
+//echo "<script language='javascript'>";
+//echo "alert('PASOO')";
+//echo "</script>";
+} catch (Exception $e) {
+throw $e;
+}
 //$sql1="DELETE FROM direccion_cliente where CLIENTE_dni = $dni;";
 //echo $sql1;
 //$con->executeQuery($sql1);
 }
 public function crearInventario($nombre, $aPaterno, $aMaterno, $fechaNacimiento,$ciudad,$cp,$colonia,$calle,$numero,$detalle/*, $DIRECCION_idDireccion*/){
 //Dia de trabajo en el ingenio.
-	try{
+try{
 $this->nombre=$nombre;
 //Produccion y demanda del dia
 $this->aPaterno = $aPaterno;
@@ -131,18 +127,15 @@ $this->calle = $calle;
 $this->numero = $numero;
 $this->detalle = $detalle;
 //$this->DIRECCION_idDireccion = $DIRECCION_idDireccion;
-
 //Inventario inicial
 /*if($this->dia == 1){
 $this->inventario_inicial = $this->produccion;
 }
 else{
 //Obteniendo el inventario del dia anterior
-
 $consulta =$this->obtenerInventarioDia($this->dia - 1);
 if(count($consulta)==0){
 throw new \Exception(sprintf("No existe un inventario para el dia %s, por tanto, no se puede seguir", $dia-1));
-
 }
 $this->inventario_inicial = $consulta[0]->inventario_final + $this->produccion;
 }*/
@@ -160,13 +153,12 @@ $this->costo_faltante = 6 * ($this->demanda - $this->inventario_inicial);
 $this->costo_inventario = 2 * (($this->inventario_inicial + $this->inventario_final) / 2);
 //Costo total
 $this->costo_total = $this->costo_produccion + $this->costo_faltante + $this->costo_inventario;
-
 //Guardando el inventario del dia*/
-echo "holaaa";
+//echo "holaaa";
 $this->guardar();
 }
 catch (Exception $e) {
-	throw $e;	
+throw $e;
 }
 }
 public function guardar(){
@@ -190,21 +182,21 @@ $this->detalle
 //ciudad,$cp,$colonia,$calle,$numero,$detall
 );*/
 //print_r($params1);
-echo "holsfdasd";
+//echo "holsfdasd";
 $sql1 = vsprintf("INSERT INTO cliente(nombre,aPaterno,aMaterno,fechaNacimiento) VALUES('%s', '%s', '%s','%s');", $params);
-echo $sql1;
+//echo $sql1;
 $con->executeUpdate(array($sql1));
 //$sql2 = vsprintf("SELECT max(dni) as dni FROM cliente;");
 //echo $sql2;
 //$CLIENTE_dni = $con->executeUpdate(array($sql2));
-echo "HASTA AQUI BIEN";
+//echo "HASTA AQUI BIEN";
 //$CLIENTE_dni = $con->executeQuery('SELECT max(dni) as dni FROM cliente;',null, __NAMESPACE__.'\ClienteModel');
 $sql= "SELECT max(dni) as dni FROM cliente;";
 $DNI = $con->executeQuery($sql);
-print_r($DNI[0]->dni);
+//print_r($DNI[0]->dni);
 $CLIENTE_dni = $DNI[0]->dni;
 //return $CLIENTE_dni;
-echo "PASO ESTO";
+//echo "PASO ESTO";
 $params1 = array(
 $this->ciudad,
 $this->cp,
@@ -214,19 +206,17 @@ $this->numero,
 $this->detalle,
 $CLIENTE_dni
 );
-print_r($params1);
+//print_r($params1);
 $sql3 = vsprintf("INSERT INTO direccion_cliente(ciudad,cp,colonia,calle,numero,detalle,CLIENTE_dni) VALUES('%s', %s, '%s', '%s', %s, '%s', %s);", $params1);
 $con->executeUpdate(array($sql3));
-echo $sql3;
-echo "PASOOO TODO";
+//echo $sql3;
+//echo "PASOOO TODO";
 } catch (Exception $e) {
-	throw $e;	
+throw $e;
 }
-
-
 }
 public function listarInventarios(){
-	try{
+try{
 $con = DBConexion::getInstance();
 if (is_null($con)) {
 throw new Exception("Error en la conexion a la base de datos, verifique",1);
@@ -234,12 +224,12 @@ throw new Exception("Error en la conexion a la base de datos, verifique",1);
 $clientes = $con->executeQuery('SELECT * FROM cliente;',null, __NAMESPACE__.'\ClienteModel');
 return $clientes;
 }
-}
 catch (Exception $e) {
-	throw $e;	
+throw $e;
+}
 }
 public function getClienteById($id){
-	try{
+try{
 $con = DBConexion::getInstance();
 if (is_null($con)) {
 throw new Exception("Error en la conexion a la base de datos, verifique",1);
@@ -248,11 +238,11 @@ $cliente = $con->executeQuery("SELECT * FROM cliente WHERE dni = ?;",array($id),
 return $cliente;
 }
 catch (Exception $e) {
-	throw $e;	
+throw $e;
 }
 }
 public function getClienteDireccionById($id){
-	try{
+try{
 $con = DBConexion::getInstance();
 if (is_null($con)) {
 throw new Exception("Error en la conexion a la base de datos, verifique",1);
@@ -262,11 +252,8 @@ $dir = $con->executeQuery("SELECT * FROM direccion_cliente WHERE CLIENTE_dni = ?
 return $dir;
 }
 catch (Exception $e) {
-	throw $e;	
+throw $e;
 }
-}
-public function updateCliente(){
-
 }
 }
 ?>
