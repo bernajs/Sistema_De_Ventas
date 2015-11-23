eb427a3b318508a75d7c948ae6786cc2560d6d1d
Lalodc <laloodc@hotmail.com>
2015-11-22 23:37:34 -0600

    Modificación al config.php
    Se cambio el valor de las variables Url y Url BASE, debido al cambio de
    nombre del proyecto

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-	define('URL', 'http://localhost:8080/proyectoEjemplo/');
+	define('URL', 'http://localhost:8080/sistema_de_ventas/');
 	define('LIBS', 'libs/');
 	define('DS', DIRECTORY_SEPARATOR);
 	define('RUTA_BASE',dirname(__FILE__));
-	define('URL_BASE', "/proyectoEjemplo");
+	define('URL_BASE', "/sistema_de_ventas");
 	
